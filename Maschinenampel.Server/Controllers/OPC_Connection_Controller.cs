9482842ba61236512b857a221b3553902c11eed2
kfeil <kilian_niklas.feil@smail.th-koeln.de>
2024-11-27 12:35:36 +0100

    Opc connection controller erstellt

---
+++ Maschinenampel.Server/Controllers/OPC_Connection_Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maschinenampel.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OPC_Connection_Controller : ControllerBase
+    {
+    }
+}
