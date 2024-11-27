b6015680b1f5cc788d2eecd5c8cdbfca5e0bfb19
Henning Steinker <henning@steinker.de>
2024-11-27 12:24:14 +0100

    Test

--- Maschinenampel.Server/Program.cs
+++ Maschinenampel.Server/Program.cs
@@ -6,7 +6,7 @@ using System.Net.WebSockets;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container. Test Henning 
+// Add services to the container.
 builder.Services.AddControllers();
 
 // Enable Swagger for API documentation
